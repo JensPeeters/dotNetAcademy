fdfab3b22b69a99ec91ecfc97e2d8d9f24a05e17
Davy <davy.cools@student.ap.be>
2019-10-23 13:47:04 +0200

    Aanpassen queries + toevoegen cursussen

--- Data layer/DbInitialiser.cs
+++ Data layer/DbInitialiser.cs
@@ -20,20 +20,50 @@ namespace Data_layer
                     new Cursus()
                     {
                         Titel = "dotNET cursus",
-                        LangeBeschrijving ="",
                         Type = ".NET",
                         Prijs = 15.45,
-                        Beschrijving = "Some example text some example text.",
-                        FotoURLCard = "https://via.placeholder.com/450x350.png/09f/fff?text=Foto van een product"
+                        Beschrijving = "Some example text some ...",
+                        FotoURLCard = "https://52bec9fb483231ac1c712343-jebebgcvzf.stackpathdns.com/wp-content/uploads/2016/05/dotnet.jpg"
+                    },
+                    new Cursus()
+                    {
+                        Titel = "dotNET cursus2",
+                        Type = ".NET",
+                        Prijs = 45.45,
+                        Beschrijving = "Some example text some ...",
+                        FotoURLCard = "https://52bec9fb483231ac1c712343-jebebgcvzf.stackpathdns.com/wp-content/uploads/2016/05/dotnet.jpg"
+                    },
+                    new Cursus()
+                    {
+                        Titel = "React cursus",
+                        Type = "Web Development",
+                        Prijs = 19.35,
+                        Beschrijving = "Some example text some ...",
+                        FotoURLCard = "https://ionicframework.com/blog/wp-content/uploads/2019/02/react-beta.png"
                     },
                     new Cursus()
                     {
                         Titel = "Angular cursus",
                         Type = "Web Development",
-                        LangeBeschrijving ="",
                         Prijs = 21.45,
-                        Beschrijving = "Some example text some example text.",
-                        FotoURLCard = "https://via.placeholder.com/450x350.png/09f/fff?text=Foto van een product"
+                        Beschrijving = "Some example text some ...",
+                        FotoURLCard = "https://miro.medium.com/max/750/1*8hD4eYuELoWAbQLNnjQ4mA.jpeg"
+                    },
+                    new Cursus()
+                    {
+                        Titel = "ReactJS cursus",
+                        Type = "Web Development",
+                        Prijs = 36.50,
+                        Beschrijving = "Some example text some ...",
+                        FotoURLCard = "https://ionicframework.com/blog/wp-content/uploads/2019/02/react-beta.png"
+                    },
+                    new Cursus()
+                    {
+                        Titel = "Vue cursus",
+                        Type = "Web Development",
+                        Prijs = 10.25,
+                        Beschrijving = "Some example text some ...",
+                        FotoURLCard = "https://miro.medium.com/max/1200/1*-PlqbnwqjqJi_EVmrhmuDQ.jpeg"
                     }
                 };
                 Traject[] trajecten =

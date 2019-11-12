423e2ea827feb0e1e66357f99bd086482b622833
Jens Peeters <jenspeeters99@hotmail.com>
2019-11-12 18:03:49 +0100

    Update SQL drop

--- Azure Commands/SQL_Drop Klanten.sql
+++ Azure Commands/SQL_Drop Klanten.sql
@@ -1,2 +1,2 @@
-ALTER TABLE [dbo].[Winkelwagens] DROP CONSTRAINT [FK_Winkelwagens_Klanten_KlantId];
+-- ALTER TABLE [dbo].[Winkelwagens] DROP CONSTRAINT [FK_Winkelwagens_Klanten_KlantId];
 DROP TABLE [dbo].[Klanten];

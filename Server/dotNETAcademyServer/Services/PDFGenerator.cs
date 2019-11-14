86e84ffe6b703307047ff233dae461a47c1e0b28
DavyCools <DavyCools@users.noreply.github.com>
2019-11-14 14:34:12 +0100

    Toevoegen mailservice + pdfgenerator
    De mail wordt nog nergens uitgevoerd, maar werkt wel. (Kan getest worden door de code die vanboven in de SendGridEmailSender in commentaar staat in een controller te steken.)

---
+++ Server/dotNETAcademyServer/Services/PDFGenerator.cs
@@ -0,0 +1,32 @@
+﻿using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+
+namespace dotNETAcademyServer.Services
+{
+    public class PDFGenerator
+    {
+        public void GeneratePDF(string fileName)
+        {
+            // Create a new PDF document
+            PdfDocument document = new PdfDocument();
+
+            // Create an empty page
+            PdfPage page = document.AddPage();
+
+            // Get an XGraphics object for drawing
+            XGraphics gfx = XGraphics.FromPdfPage(page);
+
+            // Create a font
+            XFont font = new XFont("Verdana", 20, XFontStyle.Bold);
+
+            // Draw the text
+            gfx.DrawString("Hello, World!", font, XBrushes.Black,
+              new XRect(0, 0, page.Width, page.Height),
+              XStringFormats.Center);
+
+            // Save the document...
+            document.Save("./Bestellingen/" + fileName);
+            document.Close();
+        }
+    }
+}
---
+++ Server/dotNETAcademyServer/Services/SendGridEmailSender.cs
@@ -0,0 +1,53 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace dotNETAcademyServer.Services
+{
+    public class SendGridEmailSender
+    {
+        /// <summary>
+        /// Aanroepen:
+        /// SendGridEmailSender emailSender = new SendGridEmailSender();
+        /// string mailmsg = "Mogelijk gemaakt door Davy";
+        /// emailSender.SendEmailAsync("voorbeeld@domain", "Bedankt voor je bestelling!", mailmsg, 1).Wait();
+        /// </summary>
+        PDFGenerator pdfGenerator = new PDFGenerator();
+        public async Task SendEmailAsync(string userEmail, string emailSubject, string message, int bestellingsId)
+        {
+            //var apiKey = System.Environment.GetEnvironmentVariable("SENDGRID_APIKEY");
+            var apiKey = "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";
+            var client = new SendGridClient(apiKey);
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress("noreply@dotNetAcademy.be", "dotNetAcadamy"),
+                Subject = emailSubject,
+                PlainTextContent = message,
+                HtmlContent = message
+            };
+            msg.AddTo(new EmailAddress(userEmail, ""));
+            msg.SetFooterSetting(true,
+                "<p style='color: grey;'>Dit is een automatische bevestiging van je bestelling.</p>",
+                "Dit is een automatische bevestiging van je bestelling.");
+
+            string fileName = "Factuur" + bestellingsId + ".pdf";
+            pdfGenerator.GeneratePDF(fileName);
+            byte[] pdfBytes = File.ReadAllBytes("./Bestellingen/" + fileName);
+            string pdfBase64 = Convert.ToBase64String(pdfBytes);
+
+            var bestelling = new Attachment()
+            {
+                Content = pdfBase64,
+                Type = "application/pdf",
+                Filename = fileName,
+                Disposition = "inline",
+                ContentId = "Factuur"
+            };    
+            msg.AddAttachment(bestelling);
+
+            var response = await client.SendEmailAsync(msg);
+        }
+    }
+}

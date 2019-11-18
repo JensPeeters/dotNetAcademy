80df8faa905cfbb8759ca33007b5b1494335bdba
Hector <hector.brasfalean@student.ap.be>
2019-11-18 13:49:31 +0100

    Toevoegen van filterobjecten

--- Server/Business layer/TrajectFacade.cs
+++ Server/Business layer/TrajectFacade.cs
@@ -1,4 +1,5 @@
 ﻿using Business_layer.DTO;
+using Business_layer.Filter;
 using Data_layer;
 using Data_layer.Model;
 using Data_layer.Repositories;
@@ -19,55 +20,53 @@ namespace Business_layer
             this.repository = repository;
         }
 
-        public List<TrajectDTO> GetTrajecten(string type, string titel,
-                                                 string sortBy, string direction = "asc",
-                                                 int pageSize = 16, int page = 0)
+        public List<TrajectDTO> GetTrajecten(TrajectFilter filter)
         {
             IQueryable<Traject> query = repository.GetTrajecten();
-            if (!string.IsNullOrEmpty(type))
-                query = query.Where(b => b.Type.ToLower().Contains(type.ToLower().Trim()));
+            if (!string.IsNullOrEmpty(filter.Type))
+                query = query.Where(b => b.Type.ToLower().Contains(filter.Type.ToLower().Trim()));
 
-            if (!string.IsNullOrEmpty(titel))
-                query = query.Where(b => b.Titel.ToLower().Contains(titel.ToLower().Trim()));
+            if (!string.IsNullOrEmpty(filter.Titel))
+                query = query.Where(b => b.Titel.ToLower().Contains(filter.Titel.ToLower().Trim()));
 
-            if (string.IsNullOrEmpty(sortBy))
-                sortBy = "id";
+            if (string.IsNullOrEmpty(filter.SortBy))
+                filter.SortBy = "id";
 
-            switch (sortBy.ToLower())
+            switch (filter.SortBy.ToLower())
             {
                 case "id":
-                    if (direction == "asc")
+                    if (filter.Direction == "asc")
                         query = query.OrderBy(b => b.ID);
-                    else if (direction == "desc")
+                    else if (filter.Direction == "desc")
                         query = query.OrderByDescending(b => b.ID);
                     break;
                 case "prijs":
-                    if (direction == "asc")
+                    if (filter.Direction == "asc")
                         query = query.OrderBy(b => b.Prijs);
-                    else if (direction == "desc")
+                    else if (filter.Direction == "desc")
                         query = query.OrderByDescending(b => b.Prijs);
                     break;
                 case "titel":
-                    if (direction == "asc")
+                    if (filter.Direction == "asc")
                         query = query.OrderBy(b => b.Titel);
-                    else if (direction == "desc")
+                    else if (filter.Direction == "desc")
                         query = query.OrderByDescending(b => b.Titel);
                     break;
                 case "type":
-                    if (direction == "asc")
+                    if (filter.Direction == "asc")
                         query = query.OrderBy(b => b.Type);
-                    else if (direction == "desc")
+                    else if (filter.Direction == "desc")
                         query = query.OrderByDescending(b => b.Type);
                     break;
                 default:
-                    if (direction == "asc")
+                    if (filter.Direction == "asc")
                         query = query.OrderBy(b => b.ID);
-                    else if (direction == "desc")
+                    else if (filter.Direction == "desc")
                         query = query.OrderByDescending(b => b.ID);
                     break;
             }
-            query = query.Skip(page * pageSize);
-            query = query.Take(pageSize);
+            query = query.Skip(filter.Page * filter.PageSize);
+            query = query.Take(filter.PageSize);
 
             var trajecten = new List<TrajectDTO>();
             foreach (var traject in query.ToList())

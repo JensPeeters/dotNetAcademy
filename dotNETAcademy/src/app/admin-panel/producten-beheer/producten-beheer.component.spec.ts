654f86df781bd33682a26c8eea97d3e5b1f25945
Hector <hector.brasfalean@student.ap.be>
2019-12-09 18:10:07 +0100

    fix testing

--- dotNETAcademy/src/app/admin-panel/producten-beheer/producten-beheer.component.spec.ts
+++ dotNETAcademy/src/app/admin-panel/producten-beheer/producten-beheer.component.spec.ts
@@ -1,5 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { RouterTestingModule } from '@angular/router/testing';
+
 import { ProductenBeheerComponent } from './producten-beheer.component';
 
 describe('ProductenBeheerComponent', () => {
@@ -8,6 +10,7 @@ describe('ProductenBeheerComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports:[RouterTestingModule ],
       declarations: [ ProductenBeheerComponent ]
     })
     .compileComponents();
